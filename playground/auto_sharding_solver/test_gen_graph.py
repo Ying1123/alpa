a4b8fde7e93128109cb772d9205c98e5920cf399
Ying Sheng <sqy1415@gmail.com>
2022-05-28 22:32:38 -0700

    add component for naive construction of multi-batch

---
+++ playground/auto_sharding_solver/test_gen_graph.py
@@ -0,0 +1,62 @@
+from hlo import *
+
+def get_graph(batch_size, input_dim, hidden_dim, output_dim):
+    computation = HloComputation()
+    with computation:
+        x = HloParameter((batch_size, input_dim))
+        y = HloParameter((batch_size, output_dim))
+        w1 = HloParameter((input_dim, hidden_dim))
+        w2 = HloParameter((hidden_dim, output_dim))
+
+        ## forward
+        h1 = HloDot(x, w1)
+        h2 = HloDot(h1, w2)
+        loss = HloSubtract(h2, y)
+
+        ## backward
+        coef = HloConstant(2 / batch_size / output_dim)
+        coef = HloBroadcast(coef, (batch_size, output_dim))
+        grad_loss = HloMutiply(loss, coef)
+
+        grad_w2 = HloDot(h1, grad_loss,
+                         lhs_contracting_dims=(0,),
+                         rhs_contracting_dims=(0,),)
+        new_w2 = HloSubtract(w2, grad_w2)
+        grad_h1 = HloDot(grad_loss, w2,
+                         lhs_contracting_dims=(1,),
+                         rhs_contracting_dims=(1,),)
+
+        grad_w1 = HloDot(x, grad_h1,
+                         lhs_contracting_dims=(0,),
+                         rhs_contracting_dims=(0,),)
+        new_w1 = HloSubtract(w1, grad_w1)
+        out = HloTuple((new_w1, new_w2))
+
+        ## alias
+        computation.set_alias([(w1, new_w1), (w2, new_w2)])
+
+        """
+         0: parameter.0 (128, 1024) = parameter()
+         1: parameter.1 (128, 1024) = parameter()
+         2: parameter.2 (1024, 1024) = parameter()
+         3: parameter.3 (1024, 1024) = parameter()
+         4: dot.0 (128, 1024) = dot(parameter.0, parameter.2)  lhs_con_dim=(1,), rhs_con_dim=(0,)
+         5: dot.1 (128, 1024) = dot(dot.0, parameter.3)  lhs_con_dim=(1,), rhs_con_dim=(0,)
+         6: subtract.0 (128, 1024) = subtract(dot.1, parameter.1)
+         7: constant.0 () = constant(1.52587891e-05)
+         8: broadcast.0 (128, 1024) = broadcast(constant.0)
+         9: multiply.0 (128, 1024) = multiply(subtract.0, broadcast.0)
+        10: dot.2 (1024, 1024) = dot(dot.0, multiply.0)  lhs_con_dim=(0,), rhs_con_dim=(0,)
+        11: subtract.1 (1024, 1024) = subtract(parameter.2, dot.2)
+        12: dot.3 (128, 1024) = dot(multiply.0, parameter.3)  lhs_con_dim=(1,), rhs_con_dim=(1,)
+        13: dot.4 (1024, 1024) = dot(parameter.0, dot.3)  lhs_con_dim=(0,), rhs_con_dim=(0,)
+        14: subtract.2 (1024, 1024) = subtract(parameter.2, dot.4)
+        15: tuple.0 () = tuple('subtract.2', 'subtract.1') 
+        """
+    return computation
+
+
+if __name__ == "__main__":
+    graph = get_graph(64, 256, 256, 256)
+    print(graph)
+
